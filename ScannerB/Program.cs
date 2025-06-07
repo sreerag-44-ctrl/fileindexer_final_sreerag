54297e2d90f11d7846ffe4e77f5a7cd7f2437c21
sreerag-44-ctrl <devdasanumanu@gmail.com>
2025-06-07 10:32:18 +0300

    Program.cs

--- ScannerB/Program.cs
+++ ScannerB/Program.cs
@@ -1,1 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+class AgentB
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: ScannerB <pipeName> <directoryPath>");
+            return;
+        }
+
+        string pipeName = args[0];
+        string directoryPath = args[1];
+
+        Thread t = new Thread(() => ProcessFiles(pipeName, directoryPath));
+        t.Start();
+        t.Join();
+    }
+
+    static void ProcessFiles(string pipeName, string path)
+    {
+        Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)(1 << 1); // Core 1
+
+        Dictionary<string, Dictionary<string, int>> fileWordCounts = new();
+
+        foreach (var file in Directory.GetFiles(path, "*.txt"))
+        {
+            string[] words = File.ReadAllText(file)
+                .ToLower()
+                .Split(new[] { ' ', '\r', '\n', '\t', '.', ',', ';', ':', '-', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var wordCount = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (!wordCount.ContainsKey(word))
+                    wordCount[word] = 0;
+                wordCount[word]++;
+            }
+
+            fileWordCounts[file] = wordCount;
+        }
+
+        using var pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+        pipeClient.Connect();
+
+        var json = JsonSerializer.Serialize(fileWordCounts);
+        var bytes = Encoding.UTF8.GetBytes(json);
+        pipeClient.Write(bytes, 0, bytes.Length);
+    }
+}
 
